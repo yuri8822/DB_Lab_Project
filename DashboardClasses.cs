e2519c11a4194ac83bfba135a427ac9be44f3a22
Umar.exe <61727014+yuri8822@users.noreply.github.com>
2023-05-19 19:02:13 +0500

    minor changes

--- DashboardClasses.cs
+++ DashboardClasses.cs
@@ -35,7 +35,7 @@ namespace DB_Lab_Project
         private void DashboardClasses_Load(object sender, EventArgs e)
         {
             List<TeacherDashboardClasses> teacherDashboardClasses = new List<TeacherDashboardClasses>();
-            SqlConnection conn = new SqlConnection(db.getARString()); // ITS OVER HERE! remember to define your string in the Database.cs class!
+            SqlConnection conn = new SqlConnection(db.getUmarString()); // ITS OVER HERE! remember to define your string in the Database.cs class!
             conn.Open();
             SqlCommand cmd = new SqlCommand("SELECT * FROM Class", conn);
             SqlDataReader reader = cmd.ExecuteReader();
--- Data.sql
+++ Data.sql
@@ -6,6 +6,7 @@ USE DB_L_Project;
 CREATE TABLE USER_R (
     UserID INT PRIMARY KEY,
     Name NVARCHAR(255),
+	Pass NVARCHAR(255),
     Email NVARCHAR(255),
     UserType NVARCHAR(10) CHECK (UserType IN ('Student', 'Teacher'))
 );
@@ -20,17 +21,6 @@ CREATE TABLE TEACHER_CLASS (
     UserID INT FOREIGN KEY REFERENCES USER_R(UserID),
     ClassCode INT FOREIGN KEY REFERENCES CLASS(ClassCode),
     PRIMARY KEY(UserID, ClassCode)
-);
-
-CREATE TABLE POST (
-    PostID INT PRIMARY KEY,
-    Name NVARCHAR(255),
-    Time DATETIME,
-    ClassCode INT FOREIGN KEY REFERENCES CLASS(ClassCode),
-    PostType NVARCHAR(20) CHECK (PostType IN ('Material', 'Assignment', 'Announcement')),
-    Deadline DATETIME NULL,
-    Status NVARCHAR(20) CHECK (Status IN ('Not Started', 'In Progress', 'Submitted')) NULL,
-    Grade FLOAT NULL
 );
 
 CREATE TABLE REPORT (
@@ -53,52 +43,111 @@ CREATE TABLE STUDENT_CLASS (
     PRIMARY KEY(UserID, ClassCode)
 );
 
+CREATE TABLE Announcement (
+    announce_ID INT PRIMARY KEY,
+	announce_Title NVARCHAR(255),
+	announce_Desc NVARCHAR(MAX),
+	announce_Time DATETIME,
+    ClassCode INT FOREIGN KEY REFERENCES CLASS(ClassCode)
+);
+
+CREATE TABLE Assignment (
+    ass_ID INT PRIMARY KEY,
+	ass_Title NVARCHAR(255),
+	ass_Desc NVARCHAR(MAX),
+	ass_Time DATETIME,
+	ass_Marks INT,
+    ClassCode INT FOREIGN KEY REFERENCES CLASS(ClassCode)
+);
+
+CREATE TABLE Material (
+    MT_ID INT PRIMARY KEY,
+	MT_Title NVARCHAR(255),
+	MT_Desc NVARCHAR(MAX),
+	MT_Time DATETIME,
+    ClassCode INT FOREIGN KEY REFERENCES CLASS(ClassCode)
+);
+
 -- Inserting Data
-INSERT INTO USER_R(UserID, Name, Email, UserType) VALUES
-(1, 'John Doe', 'john.doe@example.com', 'Student'),
-(2, 'Jane Smith', 'jane.smith@example.com', 'Student'),
-(3, 'Alice Johnson', 'alice.johnson@example.com', 'Teacher'),
-(4, 'Bob Williams', 'bob.williams@example.com', 'Teacher'),
-(5, 'Charlie Brown', 'charlie.brown@example.com', 'Student');
 
-INSERT INTO CLASS (ClassCode, ClassName, ManagedBy) VALUES
-(101, 'Mathematics', 3),
-(102, 'Physics', 3),
-(103, 'Chemistry', 4),
-(104, 'Biology', 4),
-(105, 'English', 3);
 
-INSERT INTO TEACHER_CLASS (UserID, ClassCode) VALUES
-(3, 101),
-(3, 102),
-(4, 103),
-(4, 104),
-(3, 105);
+-- Insert into USER_R table
+INSERT INTO USER_R (UserID, Name, Pass, Email, UserType)
+VALUES
+(1, N'John Doe', N'password1', N'john.doe@example.com', N'Student'),
+(2, N'Jane Smith', N'password2', N'jane.smith@example.com', N'Teacher'),
+(3, N'Michael Johnson', N'password3', N'michael.johnson@example.com', N'Student'),
+(4, N'Sarah Lee', N'password4', N'sarah.lee@example.com', N'Student'),
+(5, N'David Wang', N'password5', N'david.wang@example.com', N'Teacher');
 
-INSERT INTO POST (PostID, Name, Time, ClassCode, PostType, Deadline, Status, Grade) VALUES
-(1, 'Math Assignment 1', GETDATE(), 101, 'Assignment', DATEADD(DAY, 7, GETDATE()), 'Not Started', NULL),
-(2, 'Physics Material', GETDATE(), 102, 'Material', NULL, NULL, NULL),
-(3, 'Chemistry Announcement', GETDATE(), 103, 'Announcement', NULL, NULL, NULL),
-(4, 'Biology Assignment 2', GETDATE(), 104, 'Assignment', DATEADD(DAY, 14, GETDATE()), 'Not Started', NULL),
-(5, 'English Material', GETDATE(), 105, 'Material', NULL, NULL, NULL);
+-- Insert into CLASS table
+INSERT INTO CLASS (ClassCode, ClassName, ManagedBy)
+VALUES
+(101, N'English Literature', 2),
+(102, N'Mathematics', 2),
+(103, N'Physics', 2),
+(104, N'History', 2),
+(105, N'Chemistry', 2);
 
-INSERT INTO REPORT (ReportID, Time, ClassCode) VALUES
+-- Insert into TEACHER_CLASS table
+INSERT INTO TEACHER_CLASS (UserID, ClassCode)
+VALUES
+(2, 101),
+(2, 102),
+(2, 103),
+(2, 104),
+(2, 105);
+
+-- Insert into REPORT table
+INSERT INTO REPORT (ReportID, Time, ClassCode)
+VALUES
 (1, GETDATE(), 101),
 (2, GETDATE(), 102),
 (3, GETDATE(), 103),
 (4, GETDATE(), 104),
 (5, GETDATE(), 105);
 
-INSERT INTO COMMENT (ClassCode, Time, Content, UserID) VALUES
-(101, GETDATE(), 'I am having trouble with this topic.', 1),
-(102, GETDATE(), 'This material is helpful.', 2),
-(103, GETDATE(), 'Any extra resources for this topic?', 1),
-(104, GETDATE(), 'I have a question about this assignment.', 5),
-(105, GETDATE(), 'I am excited to learn this topic!', 2);
+-- Insert into COMMENT table
+INSERT INTO COMMENT (ClassCode, Time, Content, UserID)
+VALUES
+(101, GETDATE(), N'Great class!', 1),
+(101, GETDATE(), N'Excellent teaching!', 3),
+(102, GETDATE(), N'Interesting topic!', 4),
+(103, GETDATE(), N'I need help with the assignment.', 1),
+(105, GETDATE(), N'Thanks for the explanation.', 5);
 
-INSERT INTO STUDENT_CLASS (UserID, ClassCode) VALUES
+-- Insert into STUDENT_CLASS table
+INSERT INTO STUDENT_CLASS (UserID, ClassCode)
+VALUES
 (1, 101),
-(2, 102),
-(1, 103),
-(5, 104),
-(2, 105);
+(3, 101),
+(4, 101),
+(1, 102),
+(5, 102);
+
+-- Insert into Announcement table
+INSERT INTO Announcement (announce_ID, announce_Title, announce_Desc, announce_Time, ClassCode)
+VALUES
+(1, N'Welcome message', N'Welcome to the class!', GETDATE(), 101),
+(2, N'Important announcement', N'Please submit your assignments by Friday.', GETDATE(), 102),
+(3, N'Class cancellation', N'Todays class is canceled.', GETDATE(), 103),
+(4, N'Exam schedule', N'The final exam will be held on June 15th.', GETDATE(), 104),
+(5, N'Guest speaker', N'We have a guest speaker coming next week.', GETDATE(), 105);
+
+-- Insert into Assignment table
+INSERT INTO Assignment (ass_ID, ass_Title, ass_Desc, ass_Time, ass_Marks, ClassCode)
+VALUES
+(1, N'Essay Writing', N'Write an essay on the topic of globalization.', GETDATE(), 100, 101),
+(2, N'Algebraic Equations', N'Solve the given algebraic equations.', GETDATE(), 80, 102),
+(3, N'Projectile Motion', N'Calculate the trajectory of a projectile.', GETDATE(), 90, 103),
+(4, N'Historical Research', N'Research and present a historical event.', GETDATE(), 95, 104),
+(5, N'Chemical Reactions', N'Balance the given chemical equations.', GETDATE(), 85, 105);
+
+-- Insert into Material table
+INSERT INTO Material (MT_ID, MT_Title, MT_Desc, MT_Time, ClassCode)
+VALUES
+(1, N'Introduction to Literature', N'A presentation introducing the basics of literature.', GETDATE(), 101),
+(2, N'Geometry Formulas', N'A document containing important geometry formulas.', GETDATE(), 102),
+(3, N'Laws of Thermodynamics', N'A lecture recording explaining the laws of thermodynamics.', GETDATE(), 103),
+(4, N'World War II Timeline', N'A timeline infographic illustrating key events of World War II.', GETDATE(), 104),
+(5, N'Chemical Bonding', N'A PDF document explaining different types of chemical bonds.', GETDATE(), 105);
